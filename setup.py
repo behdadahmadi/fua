4e27c81d85f1d2343aa26f625a62c155f58c7053
Behdad Ahmadi <behdadahmadi@mail.com>
2016-12-03 21:31:49 +0000

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+#from distutils.core import setup
+from setuptools import setup
+setup(
+    name='fua',
+    version='1.0.0',
+    packages=[''],
+    url='https://github.com/behdadahmadi',
+    license='MIT',
+    author='Behdad Ahmadi',
+    author_email='behdadahmadi@mail.com',
+    description='Fake User-Agents of many real devices'
+)
