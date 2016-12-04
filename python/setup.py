80ef7cdbbdb716a72893a2c35957ece0a2269866
Behdad Ahmadi <behdadahmadi@mail.com>
2016-12-04 09:29:55 +0000

    Add files via upload

---
+++ python/setup.py
@@ -0,0 +1,46 @@
+
+from setuptools import setup, find_packages  # Always prefer setuptools over distutils
+from codecs import open  # To use a consistent encoding
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the relevant file
+with open(path.join(here, 'README'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='fua',
+    packages=[''],
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # http://packaging.python.org/en/latest/tutorial.html#version
+    version='1.0.2',
+
+    description='Fake User-Agents of many real devices',
+    long_description=long_description,  #this is the
+
+    # The project's main homepage.
+    url='https://github.com/behdadahmadi',
+
+    # Author details
+    author='Behdad Ahmadi',
+    author_email='behdadahmadi@mail.com',
+
+    # Choose your license
+    license='MIT',
+
+    # See https://PyPI.python.org/PyPI?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+    ],
+
+    # What does your project relate to?
+    keywords='sample setuptools development user-agent django flask user-agent fake http httplib urllib requests'
+
+
+)
